58273642981f383cd29c49c4f6138caaa08086ad
sarasagarwal90 <agarwalsaras1546@gmail.com>
2026-02-13 20:32:49 +0530

    Update LeapYaer.c

--- LeapYaer.c
+++ LeapYaer.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
+
 int main() {
-   int year;
-   printf("Enter a year: ");
-   scanf("%d", &year);
+    int year;
 
-  
-   if (year % 400 == 0) {
-      printf("%d is a leap year.", year);
-   }
-  
-   else if (year % 100 == 0) {
-      printf("%d is not a leap year.", year);
-   }
-  
-   else if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   
-   else {
-      printf("%d is not a leap year.", year);
-   }
+    printf("Enter a year: ");
+    scanf("%d", &year);
 
-   return 0;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a Leap Year.\n", year);
+    } else {
+        printf("%d is NOT a Leap Year.\n", year);
+    }
+
+    return 0;
 }
+
