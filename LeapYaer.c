c22e46703d529ea821f17e7a4236cb81bc62c244
sarasagarwal90 <agarwalsaras1546@gmail.com>
2026-02-13 20:34:51 +0530

    Update LeapYaer.c

--- LeapYaer.c
+++ LeapYaer.c
@@ -7,9 +7,9 @@ int main() {
     scanf("%d", &year);
 
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        printf("%d is a Leap Year.\n", year);
+        printf("%d is a Leap Year", year);
     } else {
-        printf("%d is NOT a Leap Year.\n", year);
+        printf("%d is NOT a Leap Year", year);
     }
 
     return 0;
